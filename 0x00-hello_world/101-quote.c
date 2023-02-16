1bc55e943ffe9b8c7939aa2ae394fbcc493c0e7e
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-16 21:14:44 +0000

    program that prints to standar error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  * main - program that prints to standard error
  *
- * Return: 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
-
