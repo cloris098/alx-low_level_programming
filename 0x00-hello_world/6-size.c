267351cabebb29ea860b7b1fa9f0c0accc5f5f22
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-16 20:07:01 +0000

    C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - program that prints the size of various types on the computer it is compiled and run on
+ * main - program that prints the size of various types on the computer
  *
- * Return: Always (0) Success
+ * Return: (0) Success
  */
 int main(void)
 {
