96069553176bda5b848178aa297dc118d3232a07
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-18 23:02:50 +0000

    prints all possible combination ofsingle digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single digits
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
