092e260a4e3aa9f2a4209052468776e46cf1e52b
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-22 03:53:34 -0800

    prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,16 @@
  */
 int print_last_digit(int n)
 {
-int remainder = n % 10;
-if (n < 0)
+int remainder;
+
+remainder = n % 10;
+
+if (remainder < 0)
 {
-n *= -1;
+remainder *= -1;
 }
 
 _putchar(remainder + '0');
+
 return (remainder);
 }
