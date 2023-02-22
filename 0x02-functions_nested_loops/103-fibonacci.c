addc7821eff9c616f2d347a186bb6d76dd5115a4
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-22 07:08:45 -0800

    prints even fibonacci numbers below 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of even fibonacci numbers not above 4000000
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int fib1 = 0;
+	int fib2 = 1;
+	int fibonacci = 0;
+	int fibsum = 0;
+
+	while (fibonacci < 4000000)
+	{
+		fibonacci = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fibonacci;
+		if (fibonacci % 2 == 0)
+		{
+			fibsum += fibonacci
+		}
+	}
+	printf("%i\n", fibsum);
+	return (0);
+}
