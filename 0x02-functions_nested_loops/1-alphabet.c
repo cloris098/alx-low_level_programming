d587fb54fa4d0b7e17ce035c13d9dd9d5a2701ab
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-21 16:55:12 -0800

    prints lowercase alphabet using function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,13 @@
 
 /**
  * main - print all alphabet in lowercase
- *
- * Return: 0 Always
  */
 
 void print_alphabet(void)
 {
 char letter;
 
-for(letter = 'a'; letter = 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
 _putchar('\n');
