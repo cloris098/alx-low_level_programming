3fe039188110b6c2df72708b42cc998471bdc459
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-23 07:32:49 -0800

    comment code to prevent infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 
 i = 0;
@@ -16,7 +17,7 @@ i = 0;
  * {
  * putchar(i);
  * }
- */
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
