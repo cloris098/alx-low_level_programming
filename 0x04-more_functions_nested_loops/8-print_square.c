c150bf7c612f4cb92d67f15fa0002834950851ad
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-23 15:01:00 -0800

    prints a square using the a character

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,19 +5,17 @@
  */
 void print_square(int size)
 {
-	int height, width;
-
-	if (size > 0)
-	{
-		for (height = 0; height < size; height++)
-		{
-			for (width = 0; width < size; width++)
-				_putchar('#');
-
-			if (height == size - 1)
-				continue; 
-			_putchar('\n');
-		}
-	}
-	_putchar('\n');
+int height, width;
+if (size > 0)
+{
+for (height = 0; height < size; height++)
+{
+for (width = 0; width < size; width++)
+_putchar('#');
+if (height == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
