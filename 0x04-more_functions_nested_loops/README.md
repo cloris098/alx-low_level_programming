5a41ed9f66b36bff6aa4d2bd6329ed5491e66c27
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-23 12:41:06 -0800

    convert to uppercase or lowercase function

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Work on more functions and nested loops
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This a header file containing declarations for prototype functions defined in the 0x02-functions_nested_loops tasks and directory
+ * This is like creating my own library with declared functions
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
