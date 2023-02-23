199327a55147e7e7bc757cf69888f5ddf3d33841
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-23 13:35:15 -0800

    prints a diagonal line using the \ character

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line on the terminal
+ * @n: number of / characters to be printed
+ */
+
+void print_diagonal(int n)
+{
+	int length, space;
+
+	if (n > 0)
+	{
+		for (length = 0; length < n; length++)
+		{
+			for (space = 0; space < length; space++)
+			{
+				_putchar(' ');
+			_putchar('\\');
+			}
+
+			if (length == n - 1)
+			{
+				continue;
+			}
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
