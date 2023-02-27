2b1eeb4789b5fb183e7977960c4b4a8a6b840599
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-27 13:48:32 -0800

    ptints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#inlcude "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string to be used
+ */
+void puts_half(char *str);
+{
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+
+	str -= (length / 2);
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
