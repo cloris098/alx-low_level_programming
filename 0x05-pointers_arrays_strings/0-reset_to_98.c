e70d5912fcbabeb372e5dd295f7edd3136c91184
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-02-27 07:49:12 -0800

    change n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - change the value of n
+ * @n: variable that its value will be changed
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
