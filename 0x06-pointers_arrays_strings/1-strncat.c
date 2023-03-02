08134cd6f863547af5c8d7ce7e1e15516fbffc2c
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-03-01 18:28:59 -0800

    concatenate string with at most n bytes from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#iclude "main.h"
+
+/**
+ * _strncat - concatenates two strings by using n bytes from @src
+ * @src: first string to concatenate
+ * @dest: second string to be concatenated to the first string
+ * @n: n bytes
+ * Return: pointer to the resulting string @dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
+	return (dest);
+}
