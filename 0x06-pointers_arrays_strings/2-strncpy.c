dcccd91105e6578a2a138baf07f7550dc6eb9c80
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-03-01 18:47:26 -0800

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: buffer store
+ * @src: source string
+ * @n: number of bytes to copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+	int src_len = 0;
+
+	while (src[index++])
+	{
+		src_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	for (index = src_len; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+
+	return (dest);
+}
