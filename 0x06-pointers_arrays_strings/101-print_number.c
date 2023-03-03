bec7035c682fa956f9fe033a8180281f299d9d8b
Emmanuel Crosby Siaw <cloris098@yahoo.com>
2023-03-03 05:38:57 -0800

    print integers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function to print integers
+ * @n: integers to print
+ * Return: 0 Always
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	 _putchar((num % 10) + '0');
+}
